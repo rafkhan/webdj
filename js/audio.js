0f09e96c55f89861cb5a20fc0a82bd481421d1bd
Rafael Khan <raf.khan.f@gmail.com>
2014-09-20 08:57:59 -0400

    Add load() play() to audiomanager

--- js/audio.js
+++ js/audio.js
@@ -7,5 +7,24 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
   _audio.context = new AudioContext();
 
+  _audio.merger = _audio.context.createChannelMerger(2);
+  _audio.merger.connect(_audio.context.destination);
+
+  var deck = {
+    A: null,
+    B: null
+  };
+
+  _audio.load = function(deckName, track) {
+    var src = _audio.context.createBufferSource();
+    src.buffer = track.buffer;
+    track.src = src;
+    deck[deckName] = track;
+  };
+
+  _audio.play = function(deckName) {
+    deck[deckName].src.start(0);
+  };
+
 })(window.audioManager = window.audioManager || {});
 
