228ba0ddcda8de83ae960ade880a184e47019295
Rafael Khan <raf.khan.f@gmail.com>
2014-09-20 09:25:35 -0400

    :(

--- js/audio.js
+++ js/audio.js
@@ -9,16 +9,16 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
   _audio.merger = _audio.context.createChannelMerger(2);
   _audio.merger.connect(_audio.context.destination);
+  connection.attachStreams.push(_audio.context.destination.stream);
 
-  var deck = {
-    A: null,
-    B: null
-  };
+  var deck = {};
 
   _audio.load = function(deckName, track) {
     var src = _audio.context.createBufferSource();
     src.buffer = track.buffer;
     track.src = src;
+    //TODO hook up effects and volume
+    src.connect(_audio.merger);
     deck[deckName] = track;
   };
 
