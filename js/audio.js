7f386533711dbf4d9943f30d3bca5f790cc05110
Rafael Khan <raf.khan.f@gmail.com>
2014-09-21 00:49:26 -0400

    Add eql power xfade function

--- js/audio.js
+++ js/audio.js
@@ -142,7 +142,12 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
   };
 
   _audio.crossFade = function(val) {
-  
+    var x = parseInt(val) / parseInt(127);
+    // Use an equal-power crossfading curve:
+    var gain1 = Math.cos(x * 0.5*Math.PI);
+    var gain2 = Math.cos((1.0 - x) * 0.5*Math.PI);
+    this.ctl1.gainNode.gain.value = gain1;
+    this.ctl2.gainNode.gain.value = gain2;
   };
 
   
